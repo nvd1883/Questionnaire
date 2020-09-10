7d92197cac8d4eab6dbeb4d03711d8f9d0ba9745
Nived Pradeep <nived.p@aspiresoftwareconsultancy.com>
2020-09-10 15:14:13 +0530

    Initial Commit

---
+++ MultipleQuestionAnswer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleQuestionAnswer
+//
+//  Created by Aspire on 10/09/20.
+//  Copyright © 2020 Nived. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
